19bfcaf37da8cc9e82bcb628cd5f48f13d7b5188
ankit-hsc <E01457@hsc.com>
2018-06-11 18:40:09 +0530

    na

--- admin/admin/src/environments/environment.ts
+++ admin/admin/src/environments/environment.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://10.11.26.71:8085/ML',
-  //apiUrl:'https://220.227.175.33:8443/ML',
+ // apiUrl: 'http://10.11.26.71:8085/ML',
+  apiUrl:'http://220.227.175.33:8443/ML',
 };
